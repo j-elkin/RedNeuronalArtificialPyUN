cf004559e2a76e651b00b69c20a332a83f424c36
mfreyeso <mayo901128@gmail.com>
2014-11-04 21:46:49 -0500

    Code to RNA
    Iniciamos....

---
+++ RedNeuronal.py
@@ -0,0 +1,11 @@
+#2014-11-04
+#Red Neuronal Artificial MLP
+#Mineria de Datos
+#Universidad Nacional de Colombia Sede Manizales
+
+#John Elkin Rendon Romero
+#Mario Fernando Reyes Ojeda
+#Santiago Cespedes Zapata
+#Alejandra Quinonez
+
+
